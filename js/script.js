29326083e7e9d2b17bdafd39d9fdae52ef4de694
Eric Aguayo <computergeek90@protonmail.com>
2024-04-18 12:34:13 -0500

    width adjustments added to numerical__row class for mobile

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
-const toggleButton = document.getElementById('btn-toggle');
+// function openWindows() {
+//     document.getElementById('menu-list').style.display = 'block';
+// }
+
+// function closeWindows() {
+//     document.getElementById('menu-list').style.display = 'none';
+// }
+
+const toggleButton = document.getElementById('hamburger-menu');
 const menuToggle = document.getElementById('menu-list');
 
 // TODO: start toggle tomorrow morning
+
+toggleButton.addEventListener('click', function(){
+    if (menuToggle) {
+        menuToggle.classList.add('show');
+    } else {
+        menuToggle.classList.remove('show');
+    }
+})
