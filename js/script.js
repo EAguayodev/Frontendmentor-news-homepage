d2302354ebbbee288d587faa4146b609cb2e70bb
Eric Aguayo <computergeek90@protonmail.com>
2025-01-11 12:13:15 -0600

    adding css/reset.css

--- js/script.js
+++ js/script.js
@@ -1,21 +1,36 @@
-const toggleButton = document.getElementById('hamburger-menu');
-const menuToggle = document.getElementById('menu-list');
-const menuClose = document.getElementById('hamburger-close');
+const toggleButton = document.getElementById("hamburger-menu"),
+  menuToggle = document.getElementById("menu-list"),
+  menuClose = document.getElementById("hamburger-close");
+toggleButton.addEventListener("click", function () {
+  document.getElementById("menu-list").style.display = menuToggle
+    ? "block"
+    : "none";
+}),
+  menuClose.addEventListener("click", function () {
+    document.getElementById("menu-list").style.display = menuToggle
+      ? "none"
+      : "block";
+  });
 
-toggleButton.addEventListener('click', function(){
-    if (menuToggle) {
-        document.getElementById('menu-list').style.display = 'block';
-    } else {
-        document.getElementById('menu-list').style.display = 'none';
-        
-    }
-})
+  
+// const toggleButton = document.getElementById('hamburger-menu');
+// const menuToggle = document.getElementById('menu-list');
+// const menuClose = document.getElementById('hamburger-close');
 
-menuClose.addEventListener('click', function(){
-    if (menuToggle) {
-        document.getElementById('menu-list').style.display = 'none';
-    } else {
-        document.getElementById('menu-list').style.display = 'block';
+// toggleButton.addEventListener('click', function(){
+//     if (menuToggle) {
+//         document.getElementById('menu-list').style.display = 'block';
+//     } else {
+//         document.getElementById('menu-list').style.display = 'none';
         
-    }
-})
+//     }
+// })
+
+// menuClose.addEventListener('click', function(){
+//     if (menuToggle) {
+//         document.getElementById('menu-list').style.display = 'none';
+//     } else {
+//         document.getElementById('menu-list').style.display = 'block';
+        
+//     }
+// })
