1de1e5eb055a0b8c7103bf1f7669d520c5cd2be0
Eric Aguayo <computergeek90@protonmail.com>
2025-01-14 22:08:32 -0600

    removing comments in javascript file to reduce file size

--- js/script.js
+++ js/script.js
@@ -11,26 +11,3 @@ toggleButton.addEventListener("click", function () {
       ? "none"
       : "block";
   });
-
-  
-// const toggleButton = document.getElementById('hamburger-menu');
-// const menuToggle = document.getElementById('menu-list');
-// const menuClose = document.getElementById('hamburger-close');
-
-// toggleButton.addEventListener('click', function(){
-//     if (menuToggle) {
-//         document.getElementById('menu-list').style.display = 'block';
-//     } else {
-//         document.getElementById('menu-list').style.display = 'none';
-        
-//     }
-// })
-
-// menuClose.addEventListener('click', function(){
-//     if (menuToggle) {
-//         document.getElementById('menu-list').style.display = 'none';
-//     } else {
-//         document.getElementById('menu-list').style.display = 'block';
-        
-//     }
-// })
